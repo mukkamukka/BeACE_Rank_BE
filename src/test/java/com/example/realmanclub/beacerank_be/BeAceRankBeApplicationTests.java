b35dbf38e1c466ca4a3f7677ae25bdaa4feb9f4d
HarenKei <seungmin1266@gmail.com>
2023-09-14 14:48:54 +0900

    First Commit

---
+++ src/test/java/com/example/realmanclub/beacerank_be/BeAceRankBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realmanclub.beacerank_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAceRankBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
