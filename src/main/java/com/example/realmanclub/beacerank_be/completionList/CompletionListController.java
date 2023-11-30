0aeaf24390a7bd63bd4ba2e8e2aa62b815ad1188
김동건 <rlaehdrjs4605@gmail.com>
2023-11-30 15:14:38 +0900

    이수 신청시 BeACE 총 점수 update와 id, BeACE code가 유효하지 않는 예외 처리를 추가했습니다.

--- src/main/java/com/example/realmanclub/beacerank_be/completionList/CompletionListController.java
+++ src/main/java/com/example/realmanclub/beacerank_be/completionList/CompletionListController.java
@@ -1,6 +1,9 @@
 package com.example.realmanclub.beacerank_be.completionList;
 
+import com.example.realmanclub.beacerank_be.Message;
+import com.example.realmanclub.beacerank_be.StatusEnum;
 import com.example.realmanclub.beacerank_be.completionList.dto.UserCompleteProgram;
+import com.example.realmanclub.beacerank_be.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +14,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CompletionListController {
 
+    private final UserService userService;
     private final CompletionListService completionListService;
 
     @PostMapping("/completeProgram")
-    public ResponseEntity<String> saveCompleteProgram(@RequestBody UserCompleteProgram userCompleteProgram) {
+    public ResponseEntity<Message> saveCompleteProgram(@RequestBody UserCompleteProgram userCompleteProgram) {
+        Message message = new Message();
+        message.setStatusEnum(StatusEnum.BAD_REQUEST);
+
+        if (!userService.duplicateId(userCompleteProgram.getId())) {
+            message.setMessage("등록되지 않은 아이디입니다.");
+            return ResponseEntity.ok(message);
+        }
+        if (!completionListService.validateBeACECode(userCompleteProgram.getCompleteCode())) {
+            message.setMessage("유효하지않는 BeACE코드입니다.");
+            return ResponseEntity.ok(message);
+        }
+
         completionListService.userComplete(userCompleteProgram);
-        return ResponseEntity.ok("신청 완료");
+        completionListService.updateUserTotalScore(userService.findUserCompleteList(userCompleteProgram.getId()), userCompleteProgram.getId());
+
+        message.setStatusEnum(StatusEnum.OK);
+        message.setMessage("이수 신청 완료");
+        message.setData(userCompleteProgram);
+
+        return ResponseEntity.ok(message);
     }
 
     @GetMapping("/findAllCompletionList")
--- src/main/java/com/example/realmanclub/beacerank_be/completionList/CompletionListService.java
+++ src/main/java/com/example/realmanclub/beacerank_be/completionList/CompletionListService.java
@@ -3,6 +3,8 @@ package com.example.realmanclub.beacerank_be.completionList;
 import com.example.realmanclub.beacerank_be.beaceProgram.BeACEProgram;
 import com.example.realmanclub.beacerank_be.beaceProgram.BeACEProgramRepository;
 import com.example.realmanclub.beacerank_be.completionList.dto.UserCompleteProgram;
+import com.example.realmanclub.beacerank_be.user.User;
+import com.example.realmanclub.beacerank_be.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CompletionListService {
 
+    private final UserRepository userRepository;
     private final CompletionListRepository completionListRepository;
     private final BeACEProgramRepository beACEProgramRepository;
 
@@ -28,5 +31,21 @@ public class CompletionListService {
 
     public List<CompletionList> findAllCompletionList() {
         return completionListRepository.findAll();
+    }
+
+    public boolean validateBeACECode(String code) {
+        return beACEProgramRepository.existsById(code);
+    }
+
+    public void updateUserTotalScore(List<CompletionList> completionLists, int studentId) {
+        User user = userRepository.findById(studentId).get();
+        int updateScore = user.getScore();
+
+        for (CompletionList completionList : completionLists) {
+            updateScore += completionList.getBeACEPoint();
+        }
+
+        user.setScore(updateScore);
+        userRepository.save(user);
     }
 }
